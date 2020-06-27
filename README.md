cf7b73cdd59914547b5bf118854ad01191d4d067
Towqeer789 <67417413+Towqeer789@users.noreply.github.com>
2020-06-27 12:55:55 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# p5.play-boilerplate
-Boiler plate for p5.play
+# bounceOffAlgorithm
+Algorithm to bounce off two objects
--- sketch.js
+++ sketch.js
@@ -1,30 +1,30 @@
-var fixedRect,movingRect;
+var fixedRect, movingRect;
 
 function setup() {
   createCanvas(1200,800);
- fixedRect=createSprite(600,400,50,80);
- movingRect=createSprite(400,200,80,30);
- fixedRect.shapeColor="green";
- movingRect.shapeColor="green";
- }
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
 
 function draw() {
   background(0,0,0);  
-  movingRect.x=World.mouseX;
-  movingRect.y=World.mouseY;
-  if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2
-    && fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2
-    && fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2
-    && movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2){
 
-    fixedRect.shapeColor="red";
-    movingRect.shapeColor="red";
-  } else {
-    fixedRect.shapeColor="green";
-    movingRect.shapeColor="green";
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+    movingRect.velocityX = movingRect.velocityX * (-1);
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
   }
-
+  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
+  }
   drawSprites();
-
-
 }
